ef1a640b35d83a85aa11a8cef8a6f587c193c814
Lihini Galagedara <navo.galagedara@gmail.com>
2019-09-24 23:53:33 +0530

    files added

---
+++ authentication/build/precache-manifest.a5aef0dbcafe19cd0aea1d1ad3cf7c78.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de836ebb4b25686bcb2b5db4dd053969",
+    "url": "/index.html"
+  },
+  {
+    "revision": "27a496e4096368fb3056",
+    "url": "/static/js/2.35171c5d.chunk.js"
+  },
+  {
+    "revision": "69773c1bdccbae645d7d",
+    "url": "/static/js/main.8b495426.chunk.js"
+  },
+  {
+    "revision": "c97149ded686eaef0ec6",
+    "url": "/static/js/runtime-main.3451c8eb.js"
+  }
+]);
